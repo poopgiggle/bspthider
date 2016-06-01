837ed1006186b47feac15770d1f404ba049c05a8
poopgiggle <poopgiggle@gmail.com>
2016-06-01 16:41:16 -0500

    initial commit

---
+++ bspthider.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name BSPTHider
+// @description Hides point tracking thread posts on BS
+// @namespace poopgiggle
+// @version 0.1
+// @include http*://www.boomershooter.com/forums/index.php?app=core&module=search&do=viewNewContent*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
+// ==/UserScript==
+
+
+$(document).ready(function() {
+    $('tr._recordRow').each(function(){
+        if( $(this).find('td h4 a')[0].text.search('Point Tracking Thread') > 0 ){ $(this).remove(); }
+     });
+});
